0bf3bad65f86af9620aa6dd93b5702f5662e84b8
Sandro Padin <sandropadin@gmail.com>
2014-09-05 14:25:16 -0500

    simplifying config

--- spec/whereabouts/config_spec.clj
+++ spec/whereabouts/config_spec.clj
@@ -5,16 +5,6 @@
 (describe "whereabouts.config"
   (with-stubs)
 
-  (context "/set-env!"
-    (it "sets the *env* atom"
-      (set-env! :development)
-      (should= :development @*env*))
-
-    (it "calls load-config!"
-      (with-redefs [load-config! (stub :load-config!)]
-        (set-env! :development)
-        (should-have-invoked :load-config!))))
-
   (context "/eval-file"
     (it "calls slurps the file and evals using read-string"
       (with-redefs [read-string (stub :read-string)
@@ -27,11 +17,4 @@
     (it "loads a file based on env argument"
       (with-redefs [eval-file (stub :eval-file)]
         (load-config-map :my-env)
-        (should-have-invoked :eval-file {:with ["src/whereabouts/environment/my-env/config.edn"]}))))
-
-  (context "/load-config!"
-    (it "sets the *config* atom by calling load-config-map"
-      (with-redefs [*env*           (atom :development)
-                    load-config-map (stub :load-config-map {:return :stubbed})]
-        (load-config!)
-        (should= :stubbed @*config*)))))
+        (should-have-invoked :eval-file {:with ["src/whereabouts/environment/my-env/config.edn"]})))))
--- src/whereabouts/config.clj
+++ src/whereabouts/config.clj
@@ -2,23 +2,13 @@
   (:require [whereabouts.server                 :as server])
   (:require [whereabouts.database.elasticsearch :as elasticsearch]))
 
-(def ^:dynamic *env*    (atom nil))
-(def ^:dynamic *config* (atom {}))
-
 (defn eval-file [f]
   (read-string (slurp f)))
 
 (defn load-config-map [env]
   (eval-file (str "src/whereabouts/environment/" (name env) "/config.edn")))
 
-(defn load-config! []
-  (reset! *config* (load-config-map @*env*)))
-
-(defn config-for [& k]
-  (get-in @*config* k))
-
-(defn set-env! [new-env]
-  (reset! *env* new-env)
-  (load-config!)
-  (server/setup! (:server @*config*))
-  (elasticsearch/setup! (:elasticsearch @*config*)))
+(defn set-env! [env]
+  (let [config (load-config-map env)]
+    (server/setup! (:server config))
+    (elasticsearch/setup! (:elasticsearch config))))
